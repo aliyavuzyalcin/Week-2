aaf8f4b5e4bd0e3c89b6cfbcd8762a320d786284
Yavuz <aliyavuzyalcin@gmail.com>
2021-05-10 01:35:53 +0300

    Week-2 on kodlama.io Java Camp

---
+++ week2/src/inheritance2/LogManager.java
@@ -0,0 +1,14 @@
+package inheritance2;
+
+public class LogManager {
+
+    public void log(int logType) {
+        if (logType == 2) {
+            System.out.println("Dosyaya loglandı");
+        } else {
+            System.out.println("Emaile loglandı");
+        }
+
+    }
+
+}
